d1dc973bac08650f8dd59805415b0a40858fe645
wilmar <wilmarandrespuertajaramillo@gmail.com>
2024-03-02 01:51:56 -0500

    Hello world C#, variables

---
+++ Hello world/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello world");
+
+// Tipos de valiables
+
+string name = "wilmar"; // Cadena de caracteres
+int age = 20; // entero
+double height = 1.75;
+bool girlfriend = false;
