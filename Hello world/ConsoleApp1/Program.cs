d3bcd8113414a1c33962448f8e03291f6a6c1c74
wilmar <wilmarandrespuertajaramillo@gmail.com>
2024-03-03 22:17:42 -0500

    Operadores relacionales

--- Hello world/ConsoleApp1/Program.cs
+++ Hello world/ConsoleApp1/Program.cs
@@ -19,3 +19,22 @@ Console.WriteLine("Ingrese su nombre:");
 inputName = Console.ReadLine();
 
 Console.WriteLine($"Hola {inputName}, Bienvenido a mi programa :)");
+
+
+// Operadores relacionales
+
+/*
+== Igual a
+!= Diferente a
+> Mayor a
+< Menor a
+>= Mayor o igual
+<= Menor o igual
+*/
+
+var (valor1, valor2, valor3) = (10, 5, 6);
+
+Console.WriteLine($"Los valores son: {valor1}, {valor2}, {valor3} ");
+Console.WriteLine($"El valor1 es mayor que el valor2: {valor1 > valor2} ");
+Console.WriteLine($"El valor3 es menor que el valor2: {valor3 < valor2} ");
+
