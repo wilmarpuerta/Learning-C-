15d6df4ffc6e3da1cf5082c5b4cf0cf78462a48f
wilmar <wilmarandrespuertajaramillo@gmail.com>
2024-03-05 21:20:49 -0500

    Ejercicio de logueo de un usuario

---
+++ Ejercicio user/Ejercicio user/Program.cs
@@ -0,0 +1,31 @@
+﻿// Ejercicio de usuario
+
+Console.WriteLine("Ingresa el nombre de usuario:");
+string user = Console.ReadLine();
+Console.WriteLine("Ingresa una contraseña que conlleve '-':");
+
+while (true)
+{
+    string? pass = Console.ReadLine();
+
+    char[] passSplit = pass.ToCharArray();
+    pass = "";
+
+    foreach (var word in passSplit)
+    {
+        pass += $"{word}-";
+    }
+
+    if (passSplit.Length >= 1)
+    {
+        Console.WriteLine("Contraseña valida");
+        Console.WriteLine($"Tu contraseña es: {pass}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("\nContraseña invalida\nVuelve a digitar contraseña");
+    }
+    
+}
+
