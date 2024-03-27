a024e36fd1a9146a15db4b04f1f761305fd287f8
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-03-27 01:55:10 -0500

    Clases y objetos

---
+++ Clases y objetos/clases/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Objeto de poderes
+
+SuperPoder dinero = new SuperPoder();
+dinero.Nombre = "Dinero";
+dinero.Descripcion "El poder del bolsillo :v";
+dinero.Nivel = NivelPoder.NivelDos;
+
+SuperPoder condicionFisica = new SuperPoder();
+condicionFisica.Nombre = "Condición física al máximo";
+condicionFisica.Descripcion "El poder de entrenar tu cuerpo al maximo";
+condicionFisica.Nivel = NivelPoder.NivelDos;
+
+// Objeto de heroe
+SuperHeroe batman = new SuperHeroe();
+
+batman.Id = 1;
+batman.Nombre = "Batman";
+batman.IdentidadSecreta = "Bruce Wayne";
+batman.Ciudad = "Ciudad Gótica";
+batman.PuedeVolar = false;
+// Creacion de lista de poderesS
+List<SuperPoder> poderesBatman = new List<SuperPoder>();
+poderesBatman.Add(dinero);
+poderesBatman.Add(condicionFisica);
+
+batman.SuperPoderes = poderesBatman;
+
+
+// Clase heroe
+class SuperHeroe {
+    public int Id;
+    public string Nombre;
+    public string IdentidadSecreta;
+    public string Ciudad;
+    public List<SuperPoder> SuperPoderes;
+    public bool PuedeVolar;
+
+}
+
+// Clase poder
+class SuperPoder{
+    public string Nombre;
+    public string Descripcion;
+    public NivelPoder Nivel;
+}
+
+// Enumeracion de poderes
+enum NivelPoder {
+    NivelUno,
+    NivelDos,
+    NivelTres
+
+}
